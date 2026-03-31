661ed857f84be93326ba099d1e5790479c3a93c8
catinasw <alexandrvasilev12rus@gmail.com>
2026-03-31 18:46:49 +0300

    made create payment method

--- src/main/java/dev/vasilyev/minipayment/services/PaymentService.java
+++ src/main/java/dev/vasilyev/minipayment/services/PaymentService.java
@@ -1,4 +1,34 @@
-package dev.vasilyev.minipayment.api.services;
+package dev.vasilyev.minipayment.services;
 
+import dev.vasilyev.minipayment.api.dto.CreatePaymentRequest;
+import dev.vasilyev.minipayment.api.dto.PaymentDto;
+import dev.vasilyev.minipayment.domain.PaymentEntity;
+import dev.vasilyev.minipayment.domain.PaymentEntityMapper;
+import dev.vasilyev.minipayment.domain.PaymentStatus;
+import dev.vasilyev.minipayment.repositories.PaymentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final PaymentEntityMapper mapper;
+
+    public PaymentService(PaymentRepository paymentRepository, PaymentEntityMapper mapper) {
+        this.paymentRepository = paymentRepository;
+        this.mapper = mapper;
+    }
+
+    @Transactional
+    public PaymentDto createPayment(CreatePaymentRequest request) {
+        PaymentEntity paymentEntity = PaymentEntity.builder()
+                .userId(request.userId())
+                .amount(request.amount())
+                .status(PaymentStatus.NEW)
+                .build();
+
+        PaymentEntity savedPayment = paymentRepository.save(paymentEntity);
+        return mapper.convertEntityToDto(savedPayment);
+    }
 }
