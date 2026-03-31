9f7112cec71f87c546eabcdae87b7296b62b515a
catinasw <alexandrvasilev12rus@gmail.com>
2026-03-31 18:47:18 +0300

    init payment controller

---
+++ src/main/java/dev/vasilyev/minipayment/api/controllers/PaymentController.java
@@ -0,0 +1,9 @@
+package dev.vasilyev.minipayment.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentController {
+}
