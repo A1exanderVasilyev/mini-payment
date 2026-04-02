2f60d99f6a272faa8acb852b09d848604ca064ee
catinasw <alexandrvasilev12rus@gmail.com>
2026-04-02 10:57:40 +0300

    implemented payment controller

--- src/main/java/dev/vasilyev/minipayment/api/controllers/PaymentController.java
+++ src/main/java/dev/vasilyev/minipayment/api/controllers/PaymentController.java
@@ -1,9 +1,26 @@
 package dev.vasilyev.minipayment.api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.vasilyev.minipayment.api.dto.CreatePaymentRequest;
+import dev.vasilyev.minipayment.api.dto.PaymentDto;
+import dev.vasilyev.minipayment.services.PaymentService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payments")
 public class PaymentController {
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping
+    public PaymentDto create(@RequestBody CreatePaymentRequest request) {
+        return paymentService.createPayment(request);
+    }
+
+    @GetMapping("/{id}")
+    public PaymentDto get(@PathVariable Long id) {
+        return paymentService.getPayment(id);
+    }
 }
